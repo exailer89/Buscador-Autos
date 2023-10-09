0925977cf14434cf657fa207f75f78152e54c330
Hugo Barrera <exailer89@gmail.com>
2023-10-08 23:29:06 -0600

    Generar un SELECT de años

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 // Variables
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+
+const max = new Date().getFullYear();
+const min = max - 10;
+
 
 // Eventoos
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { // Se ejecuta una vez el HTML este listo.
+    // Muestra los automiviles.
     mostrarAutos();
+
+    // Llena las opciones de años.
+    llenarSelect();
 });
 
 
-// Funciones
+// Funcion para mostrar los autos en el HTML
 function mostrarAutos() {
     autos.forEach(auto => {
         const autoHTML = document.createElement('p');
@@ -20,4 +29,14 @@ function mostrarAutos() {
         // Insertar en el HTML
         resultado.appendChild(autoHTML);
     });
+}
+
+// Funcion para generar los aos del select
+function llenarSelect() {
+    for( let i = max; i >= min; i-- ) {
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion); // Agrega las opciones de año al select
+    }
 }
